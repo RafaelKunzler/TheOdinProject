a6f3bf49406d9e803afe9124b57e89e70a558062
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-09-27 12:13:17 -0300

    add pc interface and chenge ui interface

--- 3.Rock Paper Scissors/code.js
+++ 3.Rock Paper Scissors/code.js
@@ -1,5 +1,7 @@
 let scorePc = 0;
 let scorePlayer = 0;
+let round = 0;
+let maxRound = 3;
 
 document.getElementById("rock").addEventListener("click", rock);
 
@@ -25,76 +27,95 @@ function scissors() {
 function getComputerChoice(){
     const random = Math.floor(Math.random() * 3) + 1;
     if (random == 1){
+        if(round > 0){
+            document.getElementById("pcRock").classList.add("clicked");
+        }
+        round++;        
         return "Rock";
     } else if (random == 2){
+        if(round > 0){
+            document.getElementById("pcPaper").classList.add("clicked");
+        }
+        round++;        
         return "Paper";
     } else{
+        if(round > 0){
+            document.getElementById("pcScissors").classList.add("clicked");
+        }
+        round++;        
         return "Scissors";
     }    
 }
 
-function playRound(playerSelection, computerSelection){
-    
-        console.log(playerSelection);
-        console.log(computerSelection); 
-        
-        if (playerSelection == "Rock"){
-            if (computerSelection == "Paper") {
-                console.log(youLose(playerSelection, computerSelection));                        
-                }
-            else if (computerSelection == "Scissors"){
-                console.log(youWin(playerSelection, computerSelection));        
-                } else { 
+function playRound(playerSelection, computerSelection){    
+    console.log(playerSelection);
+    console.log(computerSelection);             
+    if (playerSelection == "Rock"){
+        if (computerSelection == "Paper") {
+            console.log(youLose(playerSelection, computerSelection));                        
+        }
+        else if (computerSelection == "Scissors"){
+            console.log(youWin(playerSelection, computerSelection));        
+        } else { 
             console.log(draw(playerSelection));
-            }
-        } else if (playerSelection == "Paper") {
-            if (computerSelection == "Scissors") {
-                console.log(youLose(playerSelection, computerSelection));
-                }
-            else if (computerSelection == "Rock") {
-                console.log(youWin(playerSelection, computerSelection));        
-                }
-            else { 
-                console.log(draw(playerSelection));
-            }
-        } else if (playerSelection == "Scissors") {
-            if (computerSelection == "Paper") {
-                console.log(youWin(playerSelection, computerSelection));        
-                } 
-            else if (computerSelection == "Rock") {
-                console.log(youLose(playerSelection, computerSelection));
-                }  
-            else { 
-                console.log(draw(playerSelection));
-            }       
+        }
+    } else if (playerSelection == "Paper") {
+        if (computerSelection == "Scissors") {
+            console.log(youLose(playerSelection, computerSelection));
+        }
+        else if (computerSelection == "Rock") {
+            console.log(youWin(playerSelection, computerSelection));        
+        }
+        else { 
+             console.log(draw(playerSelection));
+        }
+    } else if (playerSelection == "Scissors") {
+        if (computerSelection == "Paper") {
+            console.log(youWin(playerSelection, computerSelection));        
+        } 
+        else if (computerSelection == "Rock") {
+            console.log(youLose(playerSelection, computerSelection));
         }  
+        else { 
+            console.log(draw(playerSelection));
+        }       
+        }          
         score();
+        
         console.log(scorePlayer);        
         console.log(scorePc);          
         
+}
+
+function game(){    
+    if (scorePc < maxRound){
+        if (scorePlayer < maxRound) {  
+        removeClicked();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)
+        document.getElementById("playerScore").textContent= scorePlayer;
     }
-
-    function game(){    
-        if (scorePc < 5){
-            if (scorePlayer < 5) {       
-            const computerSelection = getComputerChoice();
-            playRound(playerSelection, computerSelection)
-            document.getElementById("playerScore").textContent= scorePlayer;
-        }
+    } else {
+        round = 0;          
     }
+    endGame();              
+}
 
-
-        if (scorePlayer >= 5) {
-            win();
-            playAgain();
-        } else if (scorePc >=5) {
-            lose();
-            playAgain();
-    }       
+function endGame() {
+    if (scorePlayer >= maxRound) {
+        win();
+        playAgain();
+        removeHov(); 
+        removeClicked();
+    } else if (scorePc >= maxRound) {
+        lose();
+        playAgain();
+        removeHov(); 
+        removeClicked();
+    }  
 }
   
-function score() {
-    
+function score() {    
     const element = document.getElementById("playerScore");
     element.innerHTML = scorePlayer;
 
@@ -142,22 +163,40 @@ function lose() {
     const txt = document.getElementById("msg");
     txt.innerHTML = "" ;
     let roundMsg = document.createElement('msg');
-    roundMsg.innerHTML = "Oh no, You lost the match!";
-   
+    roundMsg.innerHTML = "Oh no, You lost the match!";   
     msg.appendChild(roundMsg);
 }
 
-function removeHover() {
-    let element = document.getElementById("rock");
-    element.classList.remove("hover");
-    let element2 = document.getElementById("paper");
-    element2.classList.remove("hover");
-    let element3 = document.getElementById("scissors");
-    element3.classList.remove("hover");
+function removeClicked(){
+    var element = document.querySelectorAll(".container");
+    for (let i = 0; i < element.length; i++) {
+        element[i].classList.remove("clicked")
+    }
 }
 
-function playAgain(){
-    removeHover();        
+function removeHov(){
+    var element = document.querySelectorAll(".hov");
+    for (let i = 0; i < element.length; i++) {
+        element[i].classList.remove("hov")
+    }
+}
+
+/*function clicked() {
+    var clicked = document.querySelectorAll(".player");
+    for(let i = 0; i < clicked.length; i++){
+        clicked[i].addEventListener("mouseover", function(e){
+            if (e.which == 1){
+                this.classList.add("clicked")
+                console.log("clicked")
+            }
+            if (e.shiftKey){
+                console.log("shift")
+            }
+        });
+    }
+}*/
+
+function playAgain(){            
     let div = document.createElement("msg")  
     let playAgain = document.createElement('div');  
     playAgain.style.display = "flex";
@@ -167,7 +206,6 @@ function playAgain(){
     btn.innerHTML = "<a href='index.html'>Play again!</a>"; 
     msg.appendChild(playAgain);
     playAgain.appendChild(btn);
-
 }
 
 game();
