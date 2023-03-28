fc17880f1d6bbfaf9693b45821b931c5ac6eab00
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-03-28 09:11:16 -0300

    fetch documents

--- 10.Restaurant Page/restaurant-page/src/pages/Menu/Menu.js
+++ 10.Restaurant Page/restaurant-page/src/pages/Menu/Menu.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useFetchDocuments } from "../../hooks/useFetchDocument"
 
 const Menu = () => {
+
+  const { documents: meals } = useFetchDocuments("meals")
+
+  console.log(meals)
   return (
     <div>
         <h1>Menu</h1>
+        {meals && meals.map((meal) => <p id={meal.id}>{meal.name}</p>)}
     </div>
   )
 }
